90798e0bd5c9ce35f6d49a03ec5c2f67454a34cb
continuingrace <continuingrace@gmail.com>
2026-08-16 01:28:04 +0900

    Refresh cache for sticky preview

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='end-credit-letter-v23';
+const CACHE='end-credit-letter-v24';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
