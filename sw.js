2c87e0432849a37ee20a0684016069ab44496f57
continuingrace <continuingrace@gmail.com>
2026-08-16 02:40:37 +0900

    Refresh original sticky preview

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='end-credit-letter-v37';
+const CACHE='end-credit-letter-v38';
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
