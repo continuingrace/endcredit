00d9ad1d20fe4e6d627df0c25b93fb47e617d62c
continuingrace <continuingrace@gmail.com>
2026-08-16 16:15:33 +0900

    Refresh PWA cache for project save

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='end-credit-letter-v46';
+const CACHE='end-credit-letter-v47';
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
